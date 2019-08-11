7e313d9086ac685176359559d9e7512da6bb357c
prafsoni <prafsoni@gmail.com>
2019-08-11 14:22:33 -0500

    [refactor] TermLevel QueryBuilders and Queries

--- Sources/ElasticSwiftQueryDSL/Queries/Builders/TermLevelQueryBuilders.swift
+++ Sources/ElasticSwiftQueryDSL/Queries/Builders/TermLevelQueryBuilders.swift
@@ -11,9 +11,10 @@ import ElasticSwiftCore
 // MARK:- Term Query Builder
 
 public class TermQueryBuilder: QueryBuilder {
-    public var field: String?
-    public var value: String?
-    public var boost: Decimal?
+    
+    private var _field: String?
+    private var _value: String?
+    private var _boost: Decimal?
     
     typealias BuilderClosure = (TermQueryBuilder) -> Void
     
@@ -24,8 +25,33 @@ public class TermQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> TermQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(value: String) -> TermQueryBuilder {
+        self._value = value
+        return self
+    }
+    
+    public func set(boost: Decimal) -> TermQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    public var value: String? {
+        return self._value
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    
     public func build() throws -> TermQuery {
-        return TermQuery(withBuilder: self)
+        return try TermQuery(withBuilder: self)
     }
 }
 
@@ -33,8 +59,8 @@ public class TermQueryBuilder: QueryBuilder {
 
 public class TermsQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var value: [String]?
+    public var _field: String?
+    public var _values: [String] = []
     
     typealias BuilderClosure = (TermsQueryBuilder) -> Void
     
@@ -45,8 +71,30 @@ public class TermsQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> TermsQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(values: String...) -> TermsQueryBuilder {
+        self._values = values
+        return self
+    }
+    
+    public func add(value: String) -> TermsQueryBuilder {
+        self._values.append(value)
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    public var values: [String] {
+        return self._values
+    }
+    
     public func build() throws -> TermsQuery {
-        return TermsQuery(withBuilder: self)
+        return try TermsQuery(withBuilder: self)
     }
 
 }
@@ -55,15 +103,15 @@ public class TermsQueryBuilder: QueryBuilder {
 
 public class RangeQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var gte: String?
-    public var gt: String?
-    public var lte: String?
-    public var lt: String?
-    public var format: String?
-    public var timeZone: String?
-    public var boost: Decimal?
-    public var relation: ShapeRelation?
+    private var _field: String?
+    private var _gte: String?
+    private var _gt: String?
+    private var _lte: String?
+    private var _lt: String?
+    private var _format: String?
+    private var _timeZone: String?
+    private var _boost: Decimal?
+    private var _relation: ShapeRelation?
     
     typealias BuilderClosure = (RangeQueryBuilder) -> Void
     
@@ -73,9 +121,83 @@ public class RangeQueryBuilder: QueryBuilder {
         self.init()
         builderClosure(self)
     }
+    
+    public func set(field: String) -> RangeQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(gte: String) -> RangeQueryBuilder {
+        self._gte = gte
+        return self
+    }
+    
+    public func set(gt: String) -> RangeQueryBuilder {
+        self._gt = gt
+        return self
+    }
+    
+    public func set(lte: String) -> RangeQueryBuilder {
+        self._lte = lte
+        return self
+    }
+    
+    public func set(lt: String) -> RangeQueryBuilder {
+        self._lt = lt
+        return self
+    }
+    
+    public func set(format: String) -> RangeQueryBuilder {
+        self._format = format
+        return self
+    }
+    
+    public func set(timeZone: String) -> RangeQueryBuilder {
+        self._timeZone = timeZone
+        return self
+    }
+    
+    public func set(boost: Decimal) -> RangeQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public func set(relation: ShapeRelation) -> RangeQueryBuilder {
+        self._relation = relation
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    public var gte: String? {
+        return self._gte
+    }
+    public var gt: String? {
+        return self._gt
+    }
+    public var lte: String? {
+        return self._lte
+    }
+    public var lt: String? {
+        return self._lt
+    }
+    public var format: String? {
+        return self._format
+    }
+    public var timeZone: String? {
+        return self._timeZone
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    public var relation: ShapeRelation? {
+        return self._relation
+    }
+    
 
     public func build() throws -> RangeQuery {
-        return RangeQuery(withBuilder: self)
+        return try RangeQuery(withBuilder: self)
     }
 }
 
@@ -83,7 +205,7 @@ public class RangeQueryBuilder: QueryBuilder {
 
 public class ExistsQueryBuilder: QueryBuilder {
     
-    public var field: String?
+    private var _field: String?
     
     typealias BuilderClosure = (ExistsQueryBuilder) -> Void
     
@@ -93,9 +215,18 @@ public class ExistsQueryBuilder: QueryBuilder {
         self.init()
         builderClosure(self)
     }
+    
+    public func set(field: String) -> ExistsQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
 
     public func build() throws -> ExistsQuery {
-        return ExistsQuery(withBuilder: self)
+        return try ExistsQuery(withBuilder: self)
     }
 }
 
@@ -103,9 +234,9 @@ public class ExistsQueryBuilder: QueryBuilder {
 
 public class PrefixQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var value: String?
-    public var boost: Decimal?
+    private var _field: String?
+    private var _value: String?
+    private var _boost: Decimal?
     
     typealias BuilderClosure = (PrefixQueryBuilder) -> Void
     
@@ -116,8 +247,34 @@ public class PrefixQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> PrefixQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(value: String) -> PrefixQueryBuilder {
+        self._value = value
+        return self
+    }
+    
+    public func set(boost: Decimal) -> PrefixQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    
+    public var value: String? {
+        return self._value
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    
     public func build() throws -> PrefixQuery {
-        return PrefixQuery(withBuilder: self)
+        return try PrefixQuery(withBuilder: self)
     }
 }
 
@@ -125,9 +282,9 @@ public class PrefixQueryBuilder: QueryBuilder {
 
 public class WildCardQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var value: String?
-    public var boost: Decimal?
+    private var _field: String?
+    private var _value: String?
+    private var _boost: Decimal?
     
     typealias BuilderClosure = (WildCardQueryBuilder) -> Void
     
@@ -138,8 +295,34 @@ public class WildCardQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> WildCardQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(value: String) -> WildCardQueryBuilder {
+        self._value = value
+        return self
+    }
+    
+    public func set(boost: Decimal) -> WildCardQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    
+    public var value: String? {
+        return self._value
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    
     public func build() throws -> WildCardQuery {
-        return WildCardQuery(withBuilder: self)
+        return try WildCardQuery(withBuilder: self)
     }
 
 }
@@ -148,11 +331,11 @@ public class WildCardQueryBuilder: QueryBuilder {
 
 public class RegexpQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var value: String?
-    public var boost: Decimal?
-    public var regexFlags: [RegexFlag]?
-    public var maxDeterminizedStates: Int?
+    private var _field: String?
+    private var _value: String?
+    private var _boost: Decimal?
+    private var _regexFlags: [RegexFlag] = []
+    private var _maxDeterminizedStates: Int?
     
     typealias BuilderClosure = (RegexpQueryBuilder) -> Void
     
@@ -163,8 +346,54 @@ public class RegexpQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> RegexpQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(value: String) -> RegexpQueryBuilder {
+        self._value = value
+        return self
+    }
+    
+    public func set(boost: Decimal) -> RegexpQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public func set(maxDeterminizedStates: Int) -> RegexpQueryBuilder {
+        self._maxDeterminizedStates = maxDeterminizedStates
+        return self
+    }
+    
+    public func set(regexFlags: RegexFlag...) -> RegexpQueryBuilder {
+        self._regexFlags = regexFlags
+        return self
+    }
+    
+    public func add(regexFlag: RegexFlag) -> RegexpQueryBuilder {
+        self._regexFlags.append(regexFlag)
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    public var value: String? {
+        return self._value
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    public var regexFlags: [RegexFlag] {
+        return self._regexFlags
+    }
+    public var maxDeterminizedStates: Int? {
+        return self._maxDeterminizedStates
+    }
+    
     public func build() throws -> RegexpQuery {
-        return RegexpQuery(withBuilder: self)
+        return try RegexpQuery(withBuilder: self)
     }
 }
 
@@ -172,13 +401,13 @@ public class RegexpQueryBuilder: QueryBuilder {
 
 public class FuzzyQueryBuilder: QueryBuilder {
     
-    public var field: String?
-    public var value: String?
-    public var boost: Decimal?
-    public var fuzziness: Int?
-    public var prefixLenght: Int?
-    public var maxExpansions: Int?
-    public var transpositions: Bool?
+    private var _field: String?
+    private var _value: String?
+    private var _boost: Decimal?
+    private var _fuzziness: Int?
+    private var _prefixLenght: Int?
+    private var _maxExpansions: Int?
+    private var _transpositions: Bool?
     
     typealias BuilderClosure = (FuzzyQueryBuilder) -> Void
     
@@ -189,8 +418,65 @@ public class FuzzyQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(field: String) -> FuzzyQueryBuilder {
+        self._field = field
+        return self
+    }
+    
+    public func set(value: String) -> FuzzyQueryBuilder {
+        self._value = value
+        return self
+    }
+    
+    public func set(boost: Decimal) -> FuzzyQueryBuilder {
+        self._boost = boost
+        return self
+    }
+    
+    public func set(fuzziness: Int) -> FuzzyQueryBuilder {
+        self._fuzziness = fuzziness
+        return self
+    }
+    
+    public func set(prefixLength: Int) -> FuzzyQueryBuilder {
+        self._prefixLenght = prefixLength
+        return self
+    }
+    
+    public func set(maxExpansions: Int) -> FuzzyQueryBuilder {
+        self._maxExpansions = maxExpansions
+        return self
+    }
+    
+    public func set(transpositions: Bool) -> FuzzyQueryBuilder {
+        self._transpositions = transpositions
+        return self
+    }
+    
+    public var field: String? {
+        return self._field
+    }
+    public var value: String? {
+        return self._value
+    }
+    public var boost: Decimal? {
+        return self._boost
+    }
+    public var fuzziness: Int? {
+        return self._fuzziness
+    }
+    public var prefixLenght: Int? {
+        return self._prefixLenght
+    }
+    public var maxExpansions: Int? {
+        return self._maxExpansions
+    }
+    public var transpositions: Bool? {
+        return self._transpositions
+    }
+    
     public func build() throws -> FuzzyQuery {
-        return FuzzyQuery(withBuilder: self)
+        return try FuzzyQuery(withBuilder: self)
     }
 
 }
@@ -199,7 +485,7 @@ public class FuzzyQueryBuilder: QueryBuilder {
 
 public class TypeQueryBuilder: QueryBuilder {
     
-    public var type: String?
+    private var _type: String?
     
     typealias BuilderClosure = (TypeQueryBuilder) -> Void
     
@@ -210,8 +496,17 @@ public class TypeQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(type: String) -> TypeQueryBuilder {
+        self._type = type
+        return self
+    }
+    
+    public var type: String? {
+        return self._type
+    }
+    
     public func build() throws -> TypeQuery {
-        return TypeQuery(withBuilder: self)
+        return try TypeQuery(withBuilder: self)
     }
 }
 
@@ -219,8 +514,8 @@ public class TypeQueryBuilder: QueryBuilder {
 
 public class IdsQueryBuilder: QueryBuilder {
     
-    public var type: String?
-    public var ids: [String]?
+    private var _type: String?
+    private var _ids: [String] = []
     
     typealias BuilderClosure = (IdsQueryBuilder) -> Void
     
@@ -231,7 +526,29 @@ public class IdsQueryBuilder: QueryBuilder {
         builderClosure(self)
     }
     
+    public func set(ids: String...) -> IdsQueryBuilder {
+        self._ids = ids
+        return self
+    }
+    
+    public func add(id: String) -> IdsQueryBuilder {
+        self._ids.append(id)
+        return self
+    }
+    public func set(type: String) -> IdsQueryBuilder {
+        self._type = type
+        return self
+    }
+    
+    public var type: String? {
+        return self._type
+    }
+    
+    public var ids: [String] {
+        return self._ids
+    }
+    
     public func build() throws -> IdsQuery {
-        return IdsQuery(withBuilder: self)
+        return try IdsQuery(withBuilder: self)
     }
 }
