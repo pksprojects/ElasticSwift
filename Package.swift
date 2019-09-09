2a9d75e586b8e915bf229bf9f87a7bfb9f233a5e
prafsoni <prafsoni@gmail.com>
2019-09-09 04:08:51 -0500

    [chore] update dependencies

--- Package.swift
+++ Package.swift
@@ -26,10 +26,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", .upToNextMajor(from: "1.0.3")),
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.1.0"))
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.7.1")),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.4.0")),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", .upToNextMajor(from: "1.1.1")),
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.1.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
